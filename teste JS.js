5ea5d99d807983c68b9773379f54192c70e3ed4e
Erick Albert Daré <erick.dare@gmail.com>
2025-09-14 03:56:47 -0300

    Delete teste JS.js

--- teste JS.js
+++
@@ -1,9 +0,0 @@
-for (i=7; i<1000; i++) {
-    //seleciona todos os números de 7 a 1000//
-
-    if (i%7===3)
-
-    //caso o resto seja diferente de 3, repete a operação com o próximo número//
-    
-    console.log(i);
-}
