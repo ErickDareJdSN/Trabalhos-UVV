03ff3a6c09056542f9fa55c66a53c9995a3ab7d7
Erick Albert Daré <erick.dare@gmail.com>
2026-03-12 20:51:13 -0300

    Add files via upload

--- 1 semestre/BD1/Modelos lógicos/drawSQL-pgsql-export-2026-03-12.sql
+++ 1 semestre/BD1/Modelos lógicos/drawSQL-pgsql-export-2026-03-12.sql
@@ -1,43 +1,37 @@
-CREATE TABLE "Curso"(
-    "cod"."curso" BIGINT NOT NULL,
-    "nome_curso" BIGINT NOT NULL,
-    "preco" BIGINT NOT NULL,
-    "cpf_prof" BIGINT NOT NULL,
-    "cpf_alu" BIGINT NOT NULL
-);
-ALTER TABLE
-    "Curso" ADD PRIMARY KEY("cod"."curso");
-ALTER TABLE
-    "Curso" ADD PRIMARY KEY("cpf_prof");
-ALTER TABLE
-    "Curso" ADD PRIMARY KEY("cpf_alu");
-CREATE TABLE "Professor"(
-    "cpf_prof" BIGINT NOT NULL,
-    "telefone" BIGINT NOT NULL,
+CREATE TABLE "Cliente"(
+    "cod"."cliente" BIGINT NOT NULL,
     "nome" BIGINT NOT NULL,
-    "endereco" BIGINT NOT NULL,
-    "sexo" BIGINT NOT NULL
+    "endereco" BIGINT NOT NULL
 );
 ALTER TABLE
-    "Professor" ADD PRIMARY KEY("cpf_prof");
-ALTER TABLE
-    "Professor" ADD CONSTRAINT "professor_telefone_unique" UNIQUE("telefone");
-CREATE TABLE "Estudante"(
-    "cpf_alu" BIGINT NOT NULL,
-    "telefone" BIGINT NOT NULL,
-    "nome" BIGINT NOT NULL,
-    "endereco" BIGINT NOT NULL,
-    "sexo" BIGINT NOT NULL
+    "Cliente" ADD PRIMARY KEY("cod"."cliente");
+CREATE TABLE "Carro"(
+    "registro" BIGINT NOT NULL,
+    "marca" BIGINT NOT NULL,
+    "modelo" BIGINT NOT NULL
 );
 ALTER TABLE
-    "Estudante" ADD PRIMARY KEY("cpf_alu");
-ALTER TABLE
-    "Estudante" ADD CONSTRAINT "estudante_telefone_unique" UNIQUE("telefone");
-CREATE TABLE "Matricula"(
-    "cod"."curso" BIGINT NOT NULL,
-    "cpf_alu" BIGINT NOT NULL
+    "Carro" ADD PRIMARY KEY("registro");
+CREATE TABLE "Apólice"(
+    "num"."apolice" BIGINT NOT NULL,
+    "cod"."cliente" BIGINT NOT NULL,
+    "registro" BIGINT NOT NULL,
+    "valor" BIGINT NOT NULL
 );
 ALTER TABLE
-    "Matricula" ADD PRIMARY KEY("cod"."curso");
+    "Apólice" ADD PRIMARY KEY("num"."apolice");
 ALTER TABLE
-    "Matricula" ADD PRIMARY KEY("cpf_alu");
+    "Apólice" ADD PRIMARY KEY("cod"."cliente");
+ALTER TABLE
+    "Apólice" ADD PRIMARY KEY("registro");
+CREATE TABLE "Acidente"(
+    "num"."acidente" BIGINT NOT NULL,
+    "registro" BIGINT NOT NULL,
+    "data" BIGINT NOT NULL,
+    "hora" BIGINT NOT NULL,
+    "local" BIGINT NOT NULL
+);
+ALTER TABLE
+    "Acidente" ADD PRIMARY KEY("num"."acidente");
+ALTER TABLE
+    "Acidente" ADD PRIMARY KEY("registro");
